6d7c9e29797ac8aa38bca070bb36ee0e0b2002aa
F.E <943507410@qq.com>
2015-12-25 18:08:18 +0800

    Update IAudioPlayer.java

--- app/src/main/java/com/pengpeng/elifeapplication/IAudioPlayer.java
+++ app/src/main/java/com/pengpeng/elifeapplication/IAudioPlayer.java
@@ -2,20 +2,20 @@ package com.pengpeng.elifeapplication;
 
 /**
  * Created by pengpeng on 15-12-8.
- */
+ */建立IAudioPlayer接口
 public interface IAudioPlayer {
 
-    public boolean isPrepared();大家是否
+    public boolean isPrepared();/是否准备好
 
-    public boolean isPaused();
+    public boolean isPaused();/是否已暂停
 
-    public void playPause();
+    public void playPause();/暂停
 
-    public void playPrevious();
+    public void playPrevious();/上一首
 
-    public void playNext();
+    public void playNext();/下一首
 
-    public void playPrepared();
+    public void playPrepared();/准备
 
-    public void play();
+    public void play();/播放
 }
